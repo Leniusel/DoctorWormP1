7305242c864efdeb5c4ff957d74c3b81f5898c7f
Ibrahim Ahmad <54756819+IamIbrahimAhmad@users.noreply.github.com>
2023-09-25 11:54:24 -0400

    Inventory

---
+++ Assets/Scripts/takeItem.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class takeItem : MonoBehaviour
+{
+    public TMP_Text txtItem;
+    private bool isOn;
+
+    private void Start()
+    {
+        txtItem.text = "";
+        isOn = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isOn)
+        {
+            print("Done");
+            gameObject.SetActive(false);
+
+            GameObject inventory = GameObject.FindGameObjectWithTag("Player");
+            inventory.GetComponent<InventorySystem>().gotItem();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        txtItem.text = "Press E to take!";
+        isOn = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        txtItem.text = "";
+        isOn = false;
+    }
+}
