3d21fdcb9a68c7fd64b5a044d41c53159d8b7312
Ibrahim Ahmad <54756819+IamIbrahimAhmad@users.noreply.github.com>
2023-10-12 16:57:47 -0400

    rain Fixed

---
+++ Assets/Scripts/rain.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rain : MonoBehaviour
+{
+    public ParticleSystem psRain;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var main = psRain.main;
+        main.simulationSpeed = 5;
+    }
+}
